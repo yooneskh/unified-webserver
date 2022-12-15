b3446decd4ae5c1a6c93624b099bac723c8dc53d
Yoones Khoshghadam <yooneskh@gmail.com>
2022-12-16 01:03:42 +0330

    fix (router): limit method values

--- unified-router.d.ts
+++ unified-router.d.ts
@@ -4,7 +4,7 @@ import { IRequestContext } from './unified-web-server.d.ts';
 export type IRouteHandlerReturn = Response | Record<string, unknown> | unknown[] | string | number | undefined;
 
 export interface IRoute {
-  method: string;
+  method: 'get' | 'post' | 'put' | 'patch' | 'delete' | 'head' | 'options';
   path: string;
   handler: (request: Request, context: IRequestContext) => IRouteHandlerReturn | Promise<IRouteHandlerReturn>;
 }
