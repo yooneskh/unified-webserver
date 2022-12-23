f2b40fe8b5b52473bd96f433eecdd37863d6c261
Yoones Khoshghadam <yooneskh@gmail.com>
2022-12-23 11:25:09 +0330

    fix (webserver): graceful recovery from request process error

--- unified-web-server.ts
+++ unified-web-server.ts
@@ -31,101 +31,113 @@ export class UnifiedWebServer extends UnifiedRouter {
 
     Deno.serve(
       async request => {
+        try {
 
-        const targetAction = patternedRoutes.find(it => it.method === request.method.toLowerCase() && it.pattern!.test(request.url));
+          const targetAction = patternedRoutes.find(it => it.method === request.method.toLowerCase() && it.pattern!.test(request.url));
 
-        if (!targetAction) {
-          return new Response(`${request.method} ${new URL(request.url).pathname} not found.`, {
-            status: 404,
-          });
-        }
-
-
-        const patternResult = targetAction.pattern.exec(request.url);
-
-        if (!patternResult) {
-          return new Response(`could not process ${request.method} ${new URL(request.url).pathname} url.`, {
-            status: 400,
-          });
-        }
-
-
-        // deno-lint-ignore no-explicit-any
-        let requestBody: any;
-        const requestHeaders = Object.fromEntries(request.headers.entries());
-
-        if (requestHeaders['content-type']?.includes('multipart/form-data')) {
-          try {
-            requestBody = await request.formData();
+          if (!targetAction) {
+            return new Response(`${request.method} ${new URL(request.url).pathname} not found.`, {
+              status: 404,
+            });
           }
-          catch {
-            return new Response(`could not parse request body as multipart form data`, {
+
+
+          const patternResult = targetAction.pattern.exec(request.url);
+
+          if (!patternResult) {
+            return new Response(`could not process ${request.method} ${new URL(request.url).pathname} url.`, {
               status: 400,
             });
           }
-        }
-        else if (requestHeaders['content-type']?.includes('application/x-www-form-urlencoded')) {
-          try {
-            requestBody = Object.fromEntries( (await request.formData()).entries() );
-          }
-          catch {
-            return new Response(`could not parse request body as url encoded form data`, {
-              status: 400,
-            });
-          }
-        }
-        else {
 
-          requestBody = await request.text();
 
-          if (requestHeaders['content-type']?.includes('application/json')) {
+          // deno-lint-ignore no-explicit-any
+          let requestBody: any;
+          const requestHeaders = Object.fromEntries(request.headers.entries());
+
+          if (requestHeaders['content-type']?.includes('multipart/form-data')) {
             try {
-              requestBody = JSON.parse(requestBody);
+              requestBody = await request.formData();
             }
             catch {
-              return new Response(`request indicated that content type is json but could not parse body as a json`, {
+              return new Response(`could not parse request body as multipart form data`, {
                 status: 400,
               });
             }
           }
+          else if (requestHeaders['content-type']?.includes('application/x-www-form-urlencoded')) {
+            try {
+              requestBody = Object.fromEntries( (await request.formData()).entries() );
+            }
+            catch {
+              return new Response(`could not parse request body as url encoded form data`, {
+                status: 400,
+              });
+            }
+          }
+          else {
 
-        }
+            requestBody = await request.text();
+
+            if (requestHeaders['content-type']?.includes('application/json')) {
+              try {
+                requestBody = JSON.parse(requestBody);
+              }
+              catch {
+                return new Response(`request indicated that content type is json but could not parse body as a json`, {
+                  status: 400,
+                });
+              }
+            }
+
+          }
 
 
-        const requestContext: IRequestContext = {
-          params: patternResult.pathname.groups,
-          headers: requestHeaders,
-          queries: Object.fromEntries(new URL(request.url).searchParams.entries()),
-          body: requestBody,
-        };
+          const requestContext: IRequestContext = {
+            params: patternResult.pathname.groups,
+            headers: requestHeaders,
+            queries: Object.fromEntries(new URL(request.url).searchParams.entries()),
+            body: requestBody,
+          };
 
 
-        let response;
+          let response;
 
-        try {
-          response = await targetAction.handler(request, requestContext);
+          try {
+            response = await targetAction.handler(request, requestContext);
+          }
+          catch (error) {
+
+            if (!this.errorHandler) {
+              throw error;
+            }
+
+            response = await this.errorHandler(error);
+
+          }
+
+
+          if (response instanceof Response) {
+            return response;
+          }
+
+          if (typeof response === 'object' || typeof response === 'undefined' || typeof response === 'boolean') {
+            return Response.json(response);
+          }
+
+          return new Response(String(response));
+
         }
         catch (error) {
 
-          if (!this.errorHandler) {
-            throw error;
-          }
+          console.error('could not process request', request);
+          console.error(error);
 
-          response = await this.errorHandler(error);
+          return new Response('Could not process request', {
+            status: 500,
+          });
 
         }
-
-
-        if (response instanceof Response) {
-          return response;
-        }
-
-        if (typeof response === 'object' || typeof response === 'undefined' || typeof response === 'boolean') {
-          return Response.json(response);
-        }
-
-        return new Response(String(response));
-
       },
       {
         port,
